7bd5d06fcb4157b8883a92e0714a995c95268dfe
James Ide <ide@jameside.com>
2016-11-04 23:29:26 -0700

    Set up git-crypt

--- tools/define_restaurants.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-# Search Yelp for the top 100 restaurants
-def get_best_restaurants(location, number):
-    base_url = 'https://api.yelp.com/v2/search/'
-    offset = 0
-    requests.get(base_url, {'term': location, 'offset': offset, 'limit': 20})
-
-# https://api.yelp.com/v2/search/?term=restaurant&location=University Ave, Palo Alto, CA
